ba436f3cab75b3109ff2fc7ad103bba5deda5ce9
Louis OHara <louisohara20@gmail.com>
2023-12-14 11:31:02 +0000

    Install dependencies and  create folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import "@fontsource-variable/inter";
 
 function App() {
   return <div className="App">Welcome to my portfolio website</div>;
